0f6184b95f4b8efa64f8d44bcc2fa17c83737af0
julvi <biotacg@gmail.com>
2016-03-09 16:10:46 +0100

    bootstrap for many columns

--- scripts_gen/bootstrap_sem.sh
+++ scripts_gen/bootstrap_sem.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
-<<'END'
+
 while getopts i: opt
 do
     case "$opt" in
         i) input=$OPTARG;;
     esac
 done
-for c in 3 4 5 6
+
+colnum=`awk -F '\t' '{print NF}' $input | sort -nu | tail -n 1`
+
+
+echo 'Genome_len_percent Accuracy' > ${input}_acc
+
+for c in `seq 3 1 $colnum`
 do
-    echo `cat $input | gawk -F "\t" -v c=$c '{if ( $2 == $c ) { tp++} nn++}END{print tp/nn}' ` #>> $output
+    p=$((($c - 2) * 10))
+    echo $p `cat $input | gawk -F "\t" -v c=$c '{if ( $2 == $c ) { tp++} nn++}END{print tp/nn}' ` >> ${input}_acc
 done
 
 
-#<<'END'
+rm -f *.tmp
+
 for s in $(echo AA | gawk '{ for ( s = 1; s<=100; s++ ) { print s }}')
 do
-    rm -f $$.tmp
+    cat $input | mkbset_long_x86_64 -seed $s -- | xC > $$.data
 
-    cat $input | mkbset_long -seed $s -- | xC > $$.data
-    for c in 3 4 5 6
+    for c in `seq 3 1 $colnum`
     do
-    echo $s `cat $$.data | gawk -F "\t" -v c=$c '{if ( $2 == $c ) { tp++} nn++}END{print tp/nn}' ` >> ${input}_bt.out
+        p=$((($c - 2) * 10))
+        echo $p `cat $$.data | gawk -F "\t" -v c=$c '{if ( $2 == $c ) { tp++} nn++}END{print tp/nn}' ` >> $p.tmp
     done
+    
 
- #   echo $s `cat $$.tmp | sort -nrk3 | head -1`
 done
-END
-filename="gr1_ann_first_maj10_covThr_alpha_gn.tab.acc"
-for c in 2 3 4 5
+
+# Make table NB! THIS WILL WORK IF *tmp FILES ARE PROPERLY ORDERED BY ls
+paste *tmp | awk '{ for (i=1;i<=NF;i+=2) $i="" }1' | \
+perl -p -e 's/^ //' > ${input}_bt_acc
+#sed -i '1i10 20 30 40 50 60 70 80 90' ${input}_bt_acc 
+
+colnum2=`awk '{print NF}' ${input}_bt_acc | sort -nu | tail -n 1`
+echo 'Genome_len_percent Accuracy_mean standard_deviation squared_error_mean' > ${input}_mean_ssd_sem
+
+for c in `seq 1 1 $colnum2`
 do
+    p=$(($c * 10))
     read mean ssd sem <<<`awk -v c=$c '{sum += $c; sumsq += $c^2} END {
-             print sum/NR, sqrt((sumsq-sum^2/NR)/(NR-1)), sqrt((sumsq-sum^2/NR)/(NR-1))/sqrt(NR)}' $filename`
-    echo $mean $ssd $sem
+             print sum/NR, sqrt((sumsq-sum^2/NR)/(NR-1)), sqrt((sumsq-sum^2/NR)/(NR-1))/sqrt(NR)}' ${input}_bt_acc`
+    echo $p $mean $ssd $sem >> ${input}_mean_ssd_sem
 done
